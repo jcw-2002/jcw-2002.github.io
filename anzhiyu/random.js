7005b6cf198ea0010a7c0b0cb7cb43af593f3bf2
jcw <zx1012386834@outlook.com>
2025-06-03 22:08:52 +0800

    Site updated: 2025-06-03 22:08:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/31/hello-world/","2025/01/15/hexo_vercel_deploy/","2025/05/22/git_update/","2025/01/01/introduce/","2025/06/01/hexo-bottom-quote-widget/","2025/06/02/swiper_fix/","2026/06/01/mlink/"];function toRandomPost(){
+var posts=["2025/05/22/git_update/","2025/05/31/hello-world/","2025/01/15/hexo_vercel_deploy/","2025/06/01/hexo-bottom-quote-widget/","2025/01/01/introduce/","2025/06/02/swiper_fix/","2026/06/01/mlink/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
